3c27905087c2ce550447e9ffdad3abe76ce33aab
John Reagan Moore <jr@jerseywatch.com>
2025-05-30 05:49:36 -0400

    csrf stuff 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception, prepend: true
+  
+  before_action :set_csrf_cookie
+  
+  private
+  
+  def set_csrf_cookie
+    cookies['XSRF-TOKEN'] = {
+      value: form_authenticity_token,
+      secure: Rails.env.production?,
+      httponly: true,
+      same_site: :lax
+    }
+  end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,6 @@ Rails.application.config.session_store :cookie_store,
   key: '_accountability_session',
   secure: Rails.env.production?,
   httponly: true,
-  same_site: :lax 
+  same_site: :lax,
+  expire_after: 1.day,
+  domain: :all 
